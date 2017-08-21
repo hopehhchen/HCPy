10d6d58853b52a3b0828f65d01f411a0b2ccbb82
hopehhchen <hhchen@cfa.harvard.edu>
2017-08-21 11:54:02 -0400

    Add comments

--- HCPy.py
+++ HCPy.py
@@ -1,6 +1,7 @@
 import numpy as np
 
-def PA(vector):
+def PositionAngle(vector):
+    # Calculate the PA of a vector, East of North
     vx, vy = vector[0], vector[1]
     PA_E = np.degrees(np.arctan2(vx, vy))
 
@@ -16,5 +17,6 @@ def PA(vector):
     return PA
 
 def PointsDist(point1, point2):
+    # Calculate the Euclidean distance between two points
 
     return np.hypot(point1[0]-point2[0], point1[1]-point2[1])
