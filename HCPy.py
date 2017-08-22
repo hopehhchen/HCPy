91dd41e4270c59b5d6eb715368a469482b2f9ea4
hopehhchen <hhchen@cfa.harvard.edu>
2017-08-22 09:55:32 -0400

    Add capability to deal with an array.

--- HCPy.py
+++ HCPy.py
@@ -1,9 +1,9 @@
 import numpy as np
 
-def PositionAngle(vector):
+def PositionAngleBase(vector):
     # Calculate the PA of a vector, East of North
     vx, vy = vector[0], vector[1]
-    PA_E = np.degrees(np.arctan2(vx, vy))
+    PA_E = np.degrees(np.arctan2(vy, vx))
 
     if (PA_E >= 90.) and (PA_E <= 180.):
         PA = PA_E - 90.
@@ -15,6 +15,17 @@ def PositionAngle(vector):
         PA = np.nan
 
     return PA
+
+def PositionAngle(vectors):
+
+    if np.size(vectors) == 2:
+        return PositionAngleBase(vectors)
+    elif np.size(vectors) > 2 and np.shape(vectors)[1] == 2:
+        return np.apply_along_axis(PositionAngleBase, 1, vectors)
+    else:
+        raise ValueError('The input has to be an (N, 2) array-like object.')
+
+
 
 def PointsDist(point1, point2):
     # Calculate the Euclidean distance between two points
