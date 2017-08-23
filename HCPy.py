7c244a5e7e542237b69a0628703374e7b687682f
hopehhchen <hhchen@cfa.harvard.edu>
2017-08-23 13:29:05 -0400

    Shape statistics for radfil objects.

--- HCPy.py
+++ HCPy.py
@@ -1,4 +1,5 @@
 import numpy as np
+from scipy.spatial import distance
 
 def PositionAngleBase(vector):
     # Calculate the PA of a vector, East of North
@@ -21,7 +22,7 @@ def PositionAngle(vectors):
     if np.size(vectors) == 2:
         return PositionAngleBase(vectors)
     elif np.size(vectors) > 2 and np.shape(vectors)[1] == 2:
-        return np.apply_along_axis(PositionAngleBase, 1, vectors)
+        return np.array(np.apply_along_axis(PositionAngleBase, 1, vectors))
     else:
         raise ValueError('The input has to be an (N, 2) array-like object.')
 
@@ -31,3 +32,18 @@ def PointsDist(point1, point2):
     # Calculate the Euclidean distance between two points
 
     return np.hypot(point1[0]-point2[0], point1[1]-point2[1])
+
+
+def PolyArea(points):
+    xx, yy = points[:, 0], points[:, 1]
+
+    return .5*np.abs(np.dot(xx, np.roll(yy,1))-np.dot(yy, np.roll(xx, 1)))
+
+def MengerCurvature(points):
+    results = []
+    for i in range(len(points)-2):
+        points3 = points[i:i+3]
+        dd = distance.cdist(points3, points3)
+        results.append(4.*PolyArea(points3)/np.sqrt(np.prod(dd[np.nonzero(dd)])))
+
+    return np.array(results)
---
+++ ShapeStatistics.py
@@ -0,0 +1,71 @@
+import time
+import numbers
+from astropy.io import fits
+import numpy as np
+import matplotlib.pyplot as plt
+
+##
+from scipy.interpolate import splprep, splev
+from scipy.spatial import distance
+#
+from HCPy import *
+
+class ShapeStatistics(object):
+    def __init__(self, radfil, samp_int = 1):
+        if hasattr(radfil, 'xbeforespline') and hasattr(radfil, 'ybeforespline'):
+            self.radfil = radfil
+            self.xbeforespline = radfil.xbeforespline
+            self.ybeforespline = radfil.ybeforespline
+        else:
+            raise ValueError('Please run build_profile before input.')
+
+        if isinstance(samp_int, numbers.Number):
+            self.samp_int = samp_int
+        else:
+            raise ValueError('samp_int needs to be a number.')
+
+        # Full spline
+        tckp, up, = splprep([self.xbeforespline, self.ybeforespline],
+                            k = 3,
+                            nest = -1)
+        self.points = np.array(splev(up, tckp)).T
+        self.fprime = np.array(splev(up, tckp, der = 1)).T
+
+        ## Quantities below are for points[1:-1].
+
+        # Distance
+        ## distance to the next point
+        distance1 = np.diagonal(distance.cdist(self.points, self.points),
+                                offset = 1)
+        self.distance = np.array([np.sum(distance1[:i])\
+                                  for i in range(1, len(distance1))])
+        self.distance = self.distance[::samp_int]
+
+        # PA
+        PA = PositionAngle(self.fprime)
+        ## Making sure that the output is continuous
+        PAcont = [PA[0]]
+        sign = False
+        for i in range(1, len(PA)):
+            ## record distcontinuous changes by sign
+            if abs(PA[i]-PA[i-1]) >= 175.:
+                sign = ~sign
+
+                if PA[i-1] >= 90.:
+                    offset = 180.
+                else:
+                    offset = -180.
+
+            ## store based on sign and the direction
+            if sign:
+                PAcont.append(PA[i]+offset)
+            else:
+                PAcont.append(PA[i])
+        self.PA = np.array(PAcont)[1:-1:samp_int]
+        ## PA changing rate
+        self.PArate = np.diff(PAcont)/distance1
+        self.PArate = self.PArate[:-1:samp_int]
+
+        # Menger Curvature
+        mcurvature = MengerCurvature(self.points)
+        self.mcurvature = mcurvature[::samp_int]
